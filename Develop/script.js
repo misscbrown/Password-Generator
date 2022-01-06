07ca9eae581b51ff0c153b48b4b2a94a0f5815e5
cee breezy <djmisscbrown@hotmail.com>
2022-01-06 12:31:57 +0000

    Added characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,10 @@
+let characterLength = 8;
+let choiceArray = [];
+
+const specialCharArr = [ '!', '@', '#', '$', '%', '^', '?', '<', '>', '=', '&', '*' ];
+const lowerCaseArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const upperCaseArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+const numberArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
