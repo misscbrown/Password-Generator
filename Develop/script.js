5e13f1590cbfde8f4618b2ac3cd9759bc440ee5b
cee breezy <djmisscbrown@hotmail.com>
2022-01-06 12:10:27 +0000

    Added starter code

---
+++ Develop/script.js
@@ -0,0 +1,13 @@
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
+// Write password to the #password input
+function writePassword() 
+
