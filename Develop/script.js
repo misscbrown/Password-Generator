8b22648e95530ea247a945224b6465477ae6ff36
cee breezy <djmisscbrown@hotmail.com>
2022-01-06 12:50:47 +0000

    Added if else statement

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,10 @@
 let characterLength = 8;
 let choiceArray = [];
 
-const specialCharArr = [ '!', '@', '#', '$', '%', '^', '?', '<', '>', '=', '&', '*' ];
-const lowerCaseArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-const upperCaseArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-const numberArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const specialChar = [ '!', '@', '#', '$', '%', '^', '?', '<', '>', '=', '&', '*' ];
+const lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+const number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -16,5 +16,20 @@ generateBtn.addEventListener("click", writePassword);
 
 
 // Write password to the #password input
-function writePassword() 
+function writePassword() {
+    let correctPrompts = getPrompts();
+    let passwordText = document.querySelector("#password");
+    
+    if(correctPrompts) {
+      let newPassword = generatePassword();
+    
+      passwordText.value = newPassword;
+      
+    } else {
+      passwordText.value = ''; 
+  
+    }
+  
+  
+  } 
 
